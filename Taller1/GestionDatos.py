91c8b578348c579b9af9b26355813e6121e1cfb3
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2023-09-08 11:38:52 -0500

    Taller1v2

--- Taller1/GestionDatos.py
+++ Taller1/GestionDatos.py
@@ -4,12 +4,12 @@ import pandas as pd
 class GestionDatos:
     def __init__(self):
         # This is the constructor.
-        string = "Hi, this is GestionDatos"
+        print("Hi, this is GestionDatos")
 
     def cargar_datos(self):
         # We add the column names for the dataFrame.
         column_names = ["Codigo", "Sexo", "Nombre", "Edad", "Ciudad"]
-        df = pd.read_csv('assets/chicos.csv', header=None, names=column_names, encoding='latin1')
+        df = pd.read_csv('chicos.csv', header=None, names=column_names, encoding='latin1')
 
         # The csv comes with numerical info for the cities and sex, we change it to strings.
         city_mapping = {1: "Bucaramanga", 2: "Girón", 3: "Florida", 4: "Piedecuesta"}
---
+++ Taller1v2/GestionDatos.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+class GestionDatos:
+
+    def __init__(self):
