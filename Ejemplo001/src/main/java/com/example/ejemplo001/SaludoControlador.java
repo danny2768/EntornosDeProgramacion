22f8be164b37267fd52b9178ddd29f033c36b816
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2023-09-15 17:01:08 -0500

    Ejemplo001

--- Ejemplo001/src/main/java/com/example/ejemplo001/SaludoControlador.java
+++ Ejemplo001/src/main/java/com/example/ejemplo001/SaludoControlador.java
@@ -2,12 +2,13 @@ package com.example.ejemplo001;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SaludoControlador{
-    @GetMapping(value = "saludo", produces = MediaType.TEXT_PLAIN_VALUE)
-    public String saludo(){
-        return "Bienvenidos al curso de Spring Boot";
+    @GetMapping(value = "saludo/{titulo}/{name}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String saludo(@PathVariable("titulo") String t, @PathVariable("name") String n){
+        return "Bienvenido " + t + " " + n + " al curso de Spring Boot";
     }
 }
