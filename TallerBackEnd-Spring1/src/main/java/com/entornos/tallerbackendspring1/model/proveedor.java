f67b973938ed81ece92a83b1d616a46bdf0fba78
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2023-09-28 06:55:59 -0500

    28/09/2023 Added proveedor model

---
+++ TallerBackEnd-Spring1/src/main/java/com/entornos/tallerbackendspring1/model/proveedor.java
@@ -0,0 +1,38 @@
+package com.entornos.tallerbackendspring1.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(schema = "tallerspring1", name = "proveedor")
+public class proveedor implements Serializable {
+    @Serial
+    private static final long serialVersionUID = -5480542936504183626L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "ciudad", nullable = false)
+    private String ciudad;
+
+    @Column(name = "direccion", nullable = false)
+    private String direccion;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "telefono", nullable = false)
+    private String telefono;
+
+    @Column(name = "nit", nullable = false)
+    private String nit;
+}
